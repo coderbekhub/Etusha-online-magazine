187806e5c26cd90455524e4061485dbff9c05207
MirzohidovMirsodiq <coderbek@mail.ru>
2023-01-30 17:07:54 +0500

    navigation done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import NavBar from './NavBar'
+import HeroBgVideo from '../img/heroBgVideo.mp4'
 
 export default function Home() {
   return (
     <>
-      <section id='homePage'>
+      <NavBar />
+
+      <section id='heroSection'>
+        <div>
+          {/* <video className='heroVideo' src={HeroBgVideo} autoPlay loop muted /> */}
+        </div>
         <div className="container">
-          <NavBar />
+          <div className='heroContent'>
+            <h2 className='hero_title'>Функция, Комфорт, Элегантность.</h2>
+            <div className='heroInfo'>
+              <h3>Подать Заявку</h3>
+              <form class="was-validated">
+                <div class="">
+                  <input type="text" class="form-control is-valid" id="validationServer01" required placeholder='Введите ваше имя' />
+                </div>
+
+                <div class="">
+                  <input type="number" class="form-control is-valid" id="validationServer01" required placeholder='Ваш номер телефона' />
+                </div>
+                
+                <div class="mb-3">
+                  <select class="form-select" required aria-label="select example">
+                    <option value="">Выберите вид услуги</option>
+                    <option value="1">Шитье</option>
+                    <option value="2">Парикмахер</option>
+                    <option value="3">Химическая Очистка</option>
+                  </select>
+                  <div class="invalid-feedback">Пример недопустимого отзыва о выборе</div>
+                </div>
+
+                <div class="mb-3">
+                  <textarea class="form-control" id="validationTextarea" placeholder="Required example textarea" required></textarea>
+                  <div class="invalid-feedback">
+                    Пожалуйста, введите сообщение в текстовую область.
+                  </div>
+                </div>
+
+
+                <div class="">
+                  <button className="btn bg-success text-warning" type="submit">Отправить</button>
+                </div>
+            </form>
+            </div>
+          </div>    
         </div>
       </section>
     </>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,67 +1,87 @@
 import React from 'react'
 import Brand from '../img/brand-removebg.png'
+import Globus from '../img/globus.png'
+import Uz from '../img/uz.png'
+import Ru from '../img/ru.png'
 
 export default function NavBar() {
   return (
     <>
-      <nav id='navBar'>
+      <nav id='navBar' className='nav_bar'>
         <div className="container">
-          <a href="#">
-          </a>
           <ul className='navList'>
             <li>
               <a href="#">
-                <img src={Brand} alt="..." width={150} height={150} />
+                <img src={Brand} alt="..." width={130} height={130} />
               </a>
             </li>
             <li><a href="#" className="nav_link">Главный</a></li>
             <li><a href="#" className="nav_link">О нас</a></li>
             <li><a href="#" className="nav_link">Наши проекты</a></li>
             <li><a href="#" className="nav_link">Контакты</a></li>
+            <div className="dropdown">
+            <button className="btn btn-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src={Globus} alt="..." width={20} height={20} />
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item nav_lang" href="#">
+                  <img src={Uz} alt="..."/> 
+                  O'zbek
+                </a></li>
+              <li>
+                <a className="dropdown-item nav_lang" href="#">
+                  <img src={Ru} alt="..."/> 
+                  Русский
+                </a>
+              </li>
+            </ul>
+          </div>
           </ul>
         </div>
       </nav>
-      <nav class="navbar bg-light fixed-top navBar_Media">
+
+      <nav className="navbar fixed-top navBar_Media ">
         <div className="container-fluid container">
-          <a className="navbar-brand" href="#">
+          <a className="navbarBrand" href="#">
             <img src={Brand} alt="..." width={60} height={60} />
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <button className="navbar-toggler link-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="media_list offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">ETUSHA</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              <h5 className="offcanvas-title mediaTitle" id="offcanvasNavbarLabel">ETUSHA</h5>
+              <button type="button" className="btn-close mediaClose" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Главный</a>
+                  <a className="nav-link mediaLink" aria-current="page" href="#">Главный</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">О нас</a>
+                  <a className="nav-link mediaLink" href="#">О нас</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Наши проекты</a>
+                  <a className="nav-link mediaLink" href="#">Наши проекты</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Контакты</a>
+                  <a className="nav-link mediaLink" href="#">Контакты</a>
                 </li>
                 <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <a className="nav-link mediaLink dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Сервисы
                   </a>
                   <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Шитье</a></li>
-                    <li><a className="dropdown-item" href="#">парикмахер</a></li>
-                    <li><a className="dropdown-item" href="#">химическая очистка</a></li>
+                    <li><a className="dropdown-item mediaDropLink" href="#">Шитье</a></li>
+                    <li><a className="dropdown-item mediaDropLink" href="#">Парикмахер</a></li>
+                    <li><a className="dropdown-item mediaDropLink" href="#">Химическая Очистка</a></li>
                   </ul>
                 </li>
               </ul>
               <form className="d-flex mt-3" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <input className="form-control me-2" type="search" placeholder="Поиск по сайту" aria-label="Search" />
+                <button className="btn btn-outline-success link-light mediaSearch" type="submit">Поиск</button>
               </form>
             </div>
           </div>
