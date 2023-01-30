60457b08d1595c7dcc75b50403ea745b38173ed6
MirzohidovMirsodiq <coderbek@mail.ru>
2023-01-30 18:06:14 +0500

    hero desktop done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,44 +9,42 @@ export default function Home() {
 
       <section id='heroSection'>
         <div>
-          {/* <video className='heroVideo' src={HeroBgVideo} autoPlay loop muted /> */}
+          <video className='heroVideo' src={HeroBgVideo} autoPlay loop muted />
         </div>
         <div className="container">
           <div className='heroContent'>
             <h2 className='hero_title'>Функция, Комфорт, Элегантность.</h2>
             <div className='heroInfo'>
-              <h3>Подать Заявку</h3>
-              <form class="was-validated">
-                <div class="">
-                  <input type="text" class="form-control is-valid" id="validationServer01" required placeholder='Введите ваше имя' />
+              <h3 className='heroInfo_title'>Подать Заявку</h3>
+              <form className="was-validated heroForm">
+                <div className="">
+                  <input type="text" className="form-control is-valid" id="validationServer01" required placeholder='Введите ваше имя' />
                 </div>
 
-                <div class="">
-                  <input type="number" class="form-control is-valid" id="validationServer01" required placeholder='Ваш номер телефона' />
+                <div className="">
+                  <input type="number" className="form-control is-valid" id="validationServer01" required placeholder='Ваш номер телефона' />
                 </div>
                 
-                <div class="mb-3">
-                  <select class="form-select" required aria-label="select example">
+                <div className="mb-3">
+                  <select className="form-select" required aria-label="select example">
                     <option value="">Выберите вид услуги</option>
                     <option value="1">Шитье</option>
                     <option value="2">Парикмахер</option>
                     <option value="3">Химическая Очистка</option>
                   </select>
-                  <div class="invalid-feedback">Пример недопустимого отзыва о выборе</div>
+                  <div className="invalid-feedback">Пример недопустимого отзыва о выборе</div>
                 </div>
 
-                <div class="mb-3">
-                  <textarea class="form-control" id="validationTextarea" placeholder="Required example textarea" required></textarea>
-                  <div class="invalid-feedback">
+                <div className="mb-3">
+                  <textarea className="form-control" id="validationTextarea" placeholder="Requifred example textarea" required></textarea>
+                  <div className="invalid-feedback">
                     Пожалуйста, введите сообщение в текстовую область.
                   </div>
                 </div>
-
-
                 <div>
-                  <button className="btn bg-success text-warning" type="submit">Отправить</button>
+                  <button className="btn bg-success heroFormSubmit" type="submit">Отправить</button>
                 </div>
-            </form>
+              </form>
             </div>
           </div>    
         </div>
