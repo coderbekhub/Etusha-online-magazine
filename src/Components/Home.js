5d078020d3eaa855fcf44bcf88d1e572a1e3a044
MirzohidovMirsodiq <coderbek@mail.ru>
2023-02-10 09:54:07 +0500

    acardion added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -210,7 +210,7 @@ export default function Home() {
       </section>
       {/* OUR TEAM END */}
 
-      <section id="faq ourTeam">
+      <section id="faq">
         <div className="container">
           <div className='whyUsTitle_content ourTeamTitle'>
             <img data-aos="fade-up" src={PatternIcon} alt="..." />
@@ -224,7 +224,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the first item's accordion body.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -234,7 +234,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -244,7 +244,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -254,7 +254,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -264,7 +264,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -274,7 +274,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
               </div>
             </div>
             <div className="accordion-item">
@@ -284,7 +284,7 @@ export default function Home() {
                 </button>
               </h2>
               <div id="flush-collapseSeven" className="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
-                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the.accordion-flush className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
               </div>
             </div>
             <div className="accordion-item">
