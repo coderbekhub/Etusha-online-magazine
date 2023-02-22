200325963b68f842af41047168e3e708ef06083d
MirzohidovMirsodiq <coderbek@mail.ru>
2023-02-22 12:42:02 +0500

    ...

--- src/Pages/ourProjects/Sewing.js
+++ src/Pages/ourProjects/Sewing.js
@@ -7,6 +7,8 @@ import SewingImg5 from '../../img/sewingImg5.png'
 import SewingImg6 from '../../img/sewingImg6.png'
 import SewingImg7 from '../../img/sewingImg7.png'
 import SewingImg8 from '../../img/sewingImg8.png'
+import SewingImg9 from '../../img/sewingImg9.png'
+import SewingImg10 from '../../img/sewingImg10.png'
 
 export default function Sewing() {
   return (
@@ -43,14 +45,22 @@ export default function Sewing() {
             <p data-aos="fade-up">Стиль неоклассика в интерьере это универсальное сочетание благородства и возвышенной утонченности с современными материалами и актуальными дизайнерскими идеями. Этот стиль являет собой интерпретацию классических традиций в оформлении жилого пространства, на современный лад.</p>
           </div>
 
-          <div className='sewingContent3'>
-            <img className='sewingImg5' src={SewingImg5} alt="..." />
-            <img className='sewingImg5' src={SewingImg6} alt="..." />
-            <div className='sewingInfo3'>
-              <img src={SewingImg7} alt="..." />
-              <img src={SewingImg8} alt="..." />
-              <p>При реализации подобного стилистического направления большую роль играют предметы мебели. Модель для подобных холлов должна быть украшена позолотой и резьбой. В качестве декора выступают мрамор, бронзовые накладки. </p>
+          <div className='sewingContent3' data-aos="fade-up">
+            <img data-aos="fade-up" className='sewingImg5' src={SewingImg5} alt="..." />
+            <img data-aos="fade-up" className='sewingImg5' src={SewingImg6} alt="..." />
+            <div data-aos="fade-up" className='sewingInfo3'>
+              <img data-aos="fade-up" src={SewingImg7} alt="..." />
+              <img data-aos="fade-up" src={SewingImg8} alt="..." />
             </div>
+              <p data-aos="fade-up">При реализации подобного стилистического направления большую роль играют предметы мебели. Модель для подобных холлов должна быть украшена позолотой и резьбой. В качестве декора выступают мрамор, бронзовые накладки. </p>
+          </div>
+
+          <div className='sewingContent4'>
+            <div className='sewingInfo4'>
+              <img src={SewingImg9} alt="..." />
+              <img src={SewingImg10} alt="..." />
+            </div>
+            <p>Резная позолота подходит для украшения рамок для зеркал. Особенно если площадь небольшая отличнымрешением станет большое зеркало. Оно помогает зрительно расширить границы комнаты, наполнить ее светом и простором. </p>
           </div>
         </div>
       </section>
