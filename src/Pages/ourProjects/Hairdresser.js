9e8bd26c9a6d0d8aaa3388c6aa5a80cc442d04a6
coderbekhub <coderbek@mail.ru>
2023-03-28 10:41:16 +0500

    ...

--- src/Pages/ourProjects/Hairdresser.js
+++ src/Pages/ourProjects/Hairdresser.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import SewingImg from '../../img/sewingImg.png'
-import SewingImg2 from '../../img/sewingImg2.png'
-import SewingImg3 from '../../img/sewingImg3.png'
-import SewingImg4 from '../../img/sewingImg4.png'
-import SewingImg5 from '../../img/sewingImg5.png'
-import SewingImg6 from '../../img/sewingImg6.png'
-import SewingImg7 from '../../img/sewingImg7.png'
-import SewingImg8 from '../../img/sewingImg8.png'
-import SewingImg9 from '../../img/sewingImg9.png'
-import SewingImg10 from '../../img/sewingImg10.png'
+// import SewingImg2 from '../../img/sewingImg2.png'
+// import SewingImg3 from '../../img/sewingImg3.png'
+// import SewingImg4 from '../../img/sewingImg4.png'
+// import SewingImg5 from '../../img/sewingImg5.png'
+// import SewingImg6 from '../../img/sewingImg6.png'
+// import SewingImg7 from '../../img/sewingImg7.png'
+// import SewingImg8 from '../../img/sewingImg8.png'
+// import SewingImg9 from '../../img/sewingImg9.png'
+// import SewingImg10 from '../../img/sewingImg10.png'
 
 function Hairdresser() {
   return (
@@ -32,7 +32,7 @@ function Hairdresser() {
             <img data-aos="fade-up" src={SewingImg} alt="..." />
           </div>
 
-          <div className='sewingImg' data-aos="fade-up">
+          {/* <div className='sewingImg' data-aos="fade-up">
             <img data-aos="fade-up" src={SewingImg2} alt="..." />
             <p data-aos="fade-up">Учитывая желание клиента, Дизайн проект квартиры разработан в современном стиле, где также присутствуют элементы неоклассики. Главным пожеланием заказчика было максимально уютное и функциональное пространство с яркими элементами.</p>
           </div>
@@ -61,7 +61,7 @@ function Hairdresser() {
               <img src={SewingImg10} alt="..." />
             </div>
             <p>Резная позолота подходит для украшения рамок для зеркал. Особенно если площадь небольшая отличнымрешением станет большое зеркало. Оно помогает зрительно расширить границы комнаты, наполнить ее светом и простором.</p>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
